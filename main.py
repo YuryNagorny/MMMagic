4fae44d7c0c0b129bbc0de249203483b5760626a
Yury <115177779+YuryNagorny@users.noreply.github.com>
2023-01-28 15:27:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,111 +1,265 @@
-import vkapi
-import vk
-import time
-from urllib.request import urlretrieve
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
-from PyQt5 import uic, QtWidgets
-import sys
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>MainWindow</class>
+ <widget class="QMainWindow" name="MainWindow">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>1140</width>
+    <height>720</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>MainWindow</string>
+  </property>
+  <widget class="QWidget" name="centralwidget">
+   <layout class="QVBoxLayout" name="verticalLayout">
+    <item>
+     <layout class="QHBoxLayout" name="horizontalLayout">
+      <item>
+       <widget class="QWidget" name="start" native="true">
+        <widget class="QWidget" name="app_id_window" native="true">
+         <property name="geometry">
+          <rect>
+           <x>300</x>
+           <y>90</y>
+           <width>521</width>
+           <height>501</height>
+          </rect>
+         </property>
+         <layout class="QVBoxLayout" name="verticalLayout_3">
+          <item>
+           <widget class="QLabel" name="label_16">
+            <property name="font">
+             <font>
+              <pointsize>14</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Введите все необходимые данные </string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QLabel" name="label_7">
+            <property name="font">
+             <font>
+              <pointsize>10</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Введите id приложения:</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QLineEdit" name="Enter_ID"/>
+          </item>
+          <item>
+           <widget class="QPushButton" name="Get_Code">
+            <property name="font">
+             <font>
+              <pointsize>10</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Получить код доступа</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QLabel" name="label_11">
+            <property name="font">
+             <font>
+              <pointsize>10</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Ваша ссылка на код доступа:</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QLineEdit" name="Code_Link"/>
+          </item>
+          <item>
+           <widget class="QLabel" name="label_12">
+            <property name="styleSheet">
+             <string notr="true">QLabel {
+  color: red;
+  font-family: Helvetica, Arial, sans-serif;
+}</string>
+            </property>
+            <property name="text">
+             <string/>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QLabel" name="label_13">
+            <property name="font">
+             <font>
+              <pointsize>10</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Введите код доступа, полученный по ссылке:</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QLineEdit" name="Enter_Code"/>
+          </item>
+          <item>
+           <widget class="QLabel" name="label_14">
+            <property name="font">
+             <font>
+              <pointsize>10</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Введите защищенный ключ:</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QLineEdit" name="Enter_Protect"/>
+          </item>
+          <item>
+           <widget class="QPushButton" name="Continue">
+            <property name="font">
+             <font>
+              <pointsize>10</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Продолжить</string>
+            </property>
+           </widget>
+          </item>
+         </layout>
+        </widget>
+       </widget>
+      </item>
+      <item>
+       <widget class="QWidget" name="menu" native="true">
+        <widget class="QWidget" name="widget_3" native="true">
+         <property name="geometry">
+          <rect>
+           <x>-300</x>
+           <y>130</y>
+           <width>591</width>
+           <height>401</height>
+          </rect>
+         </property>
+         <layout class="QVBoxLayout" name="verticalLayout_4">
+          <item>
+           <widget class="QLabel" name="label_15">
+            <property name="font">
+             <font>
+              <pointsize>16</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Добро пожаловать в VK Features!</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QPushButton" name="Album">
+            <property name="font">
+             <font>
+              <pointsize>12</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Загрузить фотоальбом</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QPushButton" name="Del_Ban_Friends">
+            <property name="font">
+             <font>
+              <pointsize>12</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Удалить заблокированных друзей</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QPushButton" name="Del_AFK_Friends">
+            <property name="font">
+             <font>
+              <pointsize>12</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Удалить друзей, которые были в сети более 1 года назад</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QPushButton" name="Download_Photo">
+            <property name="font">
+             <font>
+              <pointsize>12</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Загрузить фотографии со стены</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QPushButton" name="Del_Publics">
+            <property name="font">
+             <font>
+              <pointsize>12</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Удалить группы, не добавляющие записи более 1 года</string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QPushButton" name="Del_Noti">
+            <property name="font">
+             <font>
+              <pointsize>12</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Очистить список уведомлений </string>
+            </property>
+           </widget>
+          </item>
+          <item>
+           <widget class="QPushButton" name="Establish_Status">
+            <property name="font">
+             <font>
+              <pointsize>12</pointsize>
+             </font>
+            </property>
+            <property name="text">
+             <string>Установить статус своей страницы</string>
+            </property>
+           </widget>
+          </item>
+         </layout>
+        </widget>
+       </widget>
+      </item>
+     </layout>
+    </item>
+   </layout>
+  </widget>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
 
-
-class Authorize_Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('vk_app.ui', self)
-        self.pushButton.clicked.connect(self.authorize)
-
-    def authorize(self):
-        global vk_token
-        self.client_id = self.lineEdit.text()
-        self.redirect_uri = f'https://vk.com/editapp?id={self.client_id}'
-        session = f'https://oauth.vk.com/authorize?client_id={self.client_id}&display=page&redirect_uri={self.redirect_uri}&scope=notifications,friends&response_type=code&v=5.131'
-        self.lineEdit_2.setText(session)
-        self.vk_code = self.lineEdit_3.text()
-        self.service_key = self.lineEdit_4.text()
-        self.pushButton_2.clicked.connect(self.final)
-
-    def final(self):
-        self.client_id = self.lineEdit.text()
-        self.redirect_uri = f'https://vk.com/editapp?id={self.client_id}'
-        self.vk_code = self.lineEdit_3.text()
-        self.service_key = self.lineEdit_4.text()
-        print(self.client_id, self.redirect_uri, self.vk_code, self.service_key)
-        self.session_2 = f'https://oauth.vk.com/access_token?client_id={self.client_id}&client_secret={self.service_key}&redirect_uri={self.redirect_uri}&code={self.vk_code}'
-        self.lineEdit_5.setText(self.session_2)
-
-
-class Main_menu(QWidget):
-
-
-vk_token = None
-client_id = None
-
-
-def download_photo_album(path, token, owner_id, album_id):
-    vkapi = vk.API(token)
-    photos = vkapi.photos.get(owner_id=owner_id, album_id=album_id, count=1000, v=5.131, photo_sizes=1) # получаем список фото
-    number_photo = 0
-    for photo in photos['items']: # проходимся по фото
-        max_size_photo = photo['sizes']
-        max_size_photo.sort(key=lambda x: 'height') # берем фото с самым высоким разрешением
-        f = open(f'{path[:]}\\album_{number_photo}.jpg', 'w') # создаем файл
-        url = max_size_photo[-1]['url'] # получаем запрос изображения
-        urlretrieve(url, f"{path[:]}\\album_{number_photo}.jpg") # скачиваем фото
-        number_photo += 1
-        f.close()
-
-
-def delete_banned_friends():
-    friends_list = vkapi.friends.get(v=5.81, fields='last_seen')
-    for i in friends_list['items']:
-        if 'deactivated' in i:
-            vkapi.friends.delete(user_id=i['id'], v=5.81)
-            time.sleep(0.25)
-
-
-def delete_afk_friends():
-    friends_list = vkapi.friends.get(v=5.81, fields='last_seen')
-    for i in friends_list['items']:
-        if 'last_seen' in i:
-            if time.time() - i['last_seen']['time'] >= 31536000:
-                vkapi.friends.delete(i['id'])
-                time.sleep(0.25)
-
-
-def download_wall_photo(path, owner_id, start, end):
-    photos_count = end - start
-    number_photo = 0
-    photos = vkapi.photos.get(owner_id=owner_id, v=5.131, album_id='wall', offset=start, count=photos_count)['items'][::-1]
-    for photo in photos:
-        max_size_photo = photo['sizes']
-        max_size_photo.sort(key=lambda x: 'height')
-        f = open(f'{path[:]}\\wall_{number_photo}.jpg', 'w')
-        url = max_size_photo[-1]['url']
-        urlretrieve(url, f"{path[:]}\\wall_{number_photo}.jpg")
-        number_photo += 1
-        f.close()
-
-
-def delete_afk_groups(owner_id):
-    for i in vkapi.groups.get(owner_id=owner_id, v=5.81)['items']:
-        checker = (vkapi.wall.get(owner_id=-i, v=5.81, count=2))
-        time.sleep(0.25)
-        if time.time() - checker['items'][0]['date'] >= 31536000 and time.time() - checker['items'][1]['date'] >= 31536000:
-            vkapi.groups.leave(owner_id=checker['items'][0]['from_id'])
-
-
-def del_notifications():
-    vkapi.notifications.markAsViewed(v=5.81)
-
-
-def set_status(text):
-    vkapi.status.set(text=text)
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    main_window = Authorize_Window()
-    main_window.show()
-    vk_token = 'vk1.a.uWldt5nQXFm4zCl1wOe6ITag4pwvKfpoOpboLILOocRBc4eai_AvkVmVZKoSIz679PP3U5JPqh4qWcph0YFyRCthn_qPc_260VLHey3npGdT9VKBc6VDHZz_NXxysuj8KIfsRkk65AbTXntlMnAwwgr_3jL2hP-TD3ZodEXEgMiWyy_MdnfzAQotvxC5zXeZ'
-    vkapi = vk.API(vk_token)
-    del_notifications()
-    sys.exit(app.exec_())
